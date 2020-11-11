b7f87cf93d40ff3f491bd5e80a3348c95dd6f82c
FabianBell <fabianx.bell@gmail.com>
2020-11-11 15:21:01 +0100

    add readme to setup

--- bell_parallel/setup.py
+++ bell_parallel/setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(name='bell_parallel',
     version='0.0.1',
     description='Parallel execution utils',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Fabian Bell',
     author_email='fabianx.bell@gmail.com',
-    url='https://github.com/FabianBell/Packages',
+    url='https://github.com/FabianBell/Packages/tree/master/bell_parallel',
     license='MIT',
     packages=setuptools.find_packages(),
     classifiers=[
